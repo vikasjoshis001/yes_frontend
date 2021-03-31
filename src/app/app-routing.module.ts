7e5f6f99cc510b0417f0bdaa1a58e4514280e28a
vikasjoshis001 <vikasjoshis001@gmail.com>
2021-03-31 17:46:03 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AuthGuardService as AuthGuard } from './auth-guard.service';
+import { BusinessPageComponent } from './business-page/business-page.component';
+import { CustomersPageComponent } from './customers-page/customers-page.component';
+import { TransactionPageComponent } from './transaction-page/transaction-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "transaction/:businessId/:customerId",
+    component: TransactionPageComponent,
+    canActivate: [AuthGuard] 
+  },
+  {
+    path: "customers/:businessId",
+    component: CustomersPageComponent,
+    canActivate: [AuthGuard] 
+  },
+  {
+    path: "business",
+    component: BusinessPageComponent,
+    canActivate: [AuthGuard] 
+  },
+  {
+    path: "home",
+    component: HomePageComponent,
+  },
+  {
+    path: "",
+    component: HomePageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
