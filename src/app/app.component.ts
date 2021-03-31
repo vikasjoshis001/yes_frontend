fb6d061536a6a5b16fdb233d81fa995dca3e6fdf
vikasjoshis001 <vikasjoshis001@gmail.com>
2021-03-31 17:46:37 +0530

    Navbar created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, enableProdMode } from '@angular/core';
+import { AuthService } from "./auth.service";
+import { environment } from 'src/environments/environment';
+import { GlobalVariables } from './common/global-variable';
+enableProdMode();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'yes-frontend';
+
+  url = environment.url + "admin/"
+
+  constructor(private authService: AuthService,) { }
+
+
+  logout() {
+    this.authService.logout()
+    location.assign(environment.frontend_url + "home/")
+
+  }
+
+  backupData() {
+    this.authService.backupData()
+    console.log("Database backup sucessfull...");
+
+  }
 }
