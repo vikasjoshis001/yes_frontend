c990e2a9bf8a34e3fe11dada7a5a994a7b3985ff
vikasjoshis001 <vikasjoshis001@gmail.com>
2021-04-04 09:02:33 +0530

    Homepage Page Formatting

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { environment } from 'src/environments/environment';
 import { AuthService } from "../auth.service";
-import { GlobalVariables } from '../common/global-variable';
 import { Router } from "@angular/router";
 
 @Component({
@@ -14,33 +12,29 @@ export class HomePageComponent implements OnInit {
 
   token;
   msg;
-  show = GlobalVariables.show_model;
 
   constructor(private http: HttpClient, private authService: AuthService, private router: Router) { }
 
+  // Login Api Call
   login(data) {
-    console.log("Logging in...")
-    return this.authService.login(data.value).subscribe((result) => {
-      console.log("Successfully Logged in....");
-      this.token = result;
-      if (this.token.msg == "Invalid Credentials") {
-        this.msg = "**Invalid Username or Password";
-      }
-      else {
-        this.token = this.token.data['token']
-        sessionStorage.setItem('token', this.token);
-        this.router.navigate(['/business']);
-      }
-    })
+    try {
+      this.authService.login(data.value).subscribe((result) => {
+        console.log("Successfully Logged in....");
+        this.token = result;
+        if (this.token.msg == "Invalid Credentials") {
+          this.msg = "**Invalid Username or Password";
+        }
+        else {
+          this.token = this.token.data['token']
+          sessionStorage.setItem('token', this.token);
+          this.router.navigate(['/business']);
+        }
+      })
+    }
+    catch (Error) {
+      alert("Network Error")
+    }
   }
-
-  onClose() {
-    GlobalVariables.show_model = false;
-    this.show = GlobalVariables.show_model;
-
-  }
-
-
 
   ngOnInit(): void {
   }
